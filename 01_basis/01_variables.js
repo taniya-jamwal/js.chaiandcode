4336c43d241bc072e53127e25f4e62299cae118c
Taniya Jamwal <117116158+taniya-jamwal@users.noreply.github.com>
2024-01-25 09:37:34 +0000

    config commit done

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,20 @@
+const accountID=144556;
+let accountEmail="tai@gmail.com";
+var accountPass="123";
+accountCity="Jaipur"
+let accountState;//undefined
+
+//accountId=2; //not allowed 
+
+accountEmail="hch@gmail.com"
+accountPass="232244"
+accountCity="tumkur"
+console.log(accountID);
+
+
+/*
+prefer not to use var
+becauuse of issue in block scope and functional scope
+*/
+
+console.table([accountID,accountEmail,accountCity,accountState])
---
+++ 01_basis/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3
+    
+    +
+    3); //code readibility shoould be high
+
+console.log("taniya");
+
+
+let name="taniya"
+let age=20 //
+let isLoggedIn=false //boolean
+let state //undefined => abi value assigned nahi hue ha
+let stat=null
+
+//PRIMITIVE DATATYPE:
+//number=>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+//symbol =>unique
+
+
+
+//OBJECT:
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
