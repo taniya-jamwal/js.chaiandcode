ed174af8d306e39d8ebf7b41438ca838f5a869e8
Taniya Jamwal <117116158+taniya-jamwal@users.noreply.github.com>
2024-01-25 09:56:15 +0000

    conversion of datatype

---
+++ 01_basis/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="tan"
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber=Number(score);
+console.log(typeof ValueInNumber);
+console.log( ValueInNumber);
+
+//"33" =>33
+//"33abc" =>NAN
+// true =>1; false=>0
+
+
+let isLoggedIn= 1
+
+let booleanISLoggedIn=Boolean(isLoggedIn)
+console.log(booleanISLoggedIn);
+
+//1 =>true; 0=>false
+
+//"" =>false
+//"taniya" =>true
+
+let someNumber= 33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
