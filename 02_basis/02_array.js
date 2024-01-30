94452f0eebad61393ccbca7e704940ec87fd0824
Taniya Jamwal <117116158+taniya-jamwal@users.noreply.github.com>
2024-01-30 18:12:08 +0000

    done with arrays

---
+++ 02_basis/02_array.js
@@ -0,0 +1,24 @@
+const marvel_heros=["thor","Ironman","spiderman"]
+const dc_heros=["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allmarvel_heros=marvel_heros.concat(dc_heros);
+// console.log(allmarvel_heros);
+
+const all_new_heros=[...marvel_heros,...dc_heros]
+
+
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array=another_array.flat(Infinity)
+console.log(real_another_array);
+console.log(Array.isArray("taniya"))
+console.log(Array.from("taniya"));
+
+console.log(Array.from({name:"taniya"})) //interesting
+
+
