777861e8dc687195f3f7b932cf864ac1d7626e81
Taniya Jamwal <117116158+taniya-jamwal@users.noreply.github.com>
2024-01-31 20:00:07 +0000

    scopes

--- 02_scopes.js
+++ 02_scopes.js
@@ -1,8 +1,18 @@
 // let var const
 
-let a=10
-const b=120
-var c=30
+
+{} //curly braces hotte ha scope 
+if(true)
+{
+    let a=10
+    const b=120
+    var c=30
+}
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
 
 console.log(a)
 console.log(b)
