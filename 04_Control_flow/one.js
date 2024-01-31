b615be4f8c592da5acbfa1535e49b3178f79b451
Taniya Jamwal <117116158+taniya-jamwal@users.noreply.github.com>
2024-01-31 22:32:51 +0000

    04_Control_flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+// if(Condition)
+// {
+
+// }
+// const isUserLoggedIn=true
+// if(2==="2") // output will be excuted but if we will do === it will give nothing loop ka ande he nahi jaye ga
+// {
+// console.log("executed");
+// }
+
+//++++++++++++++++
+
+// comparision operator
+// < , > ,<= , == ,!= , ===(check type also), !==(strict checking)
+
+// const scrore=200
+// if(scrore>100)
+// {
+//     const power="fly"
+//     console.log(`user power:${power}`);
+// }
+// console.log(`user power:${power}`);
+
+const balance=1000
+// if(balance>=500) console.log("test"); //implicit scope //no multiple lines but using comma we can do
