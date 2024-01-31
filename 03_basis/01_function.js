68e06da9d619f37964f16e47030e86c4700b8ae1
Taniya Jamwal <117116158+taniya-jamwal@users.noreply.github.com>
2024-01-31 19:41:47 +0000

    donw with function1

--- 03_basis/01_function.js
+++ 03_basis/01_function.js
@@ -24,15 +24,43 @@
 
 // console.log(result);
 
-function loginUserMessage(username="tan")
-{
-    if (!username) {
-        console.log("please enter a username");
-        return
+// function loginUserMessage(username="tan")
+// {
+//     if (!username) {
+//         console.log("please enter a username");
+//         return
         
-    }
-    return `${username} just logged in`
-}
+//     }
+//     return `${username} just logged in`
+// }
 
-// console.log(loginUserMessage("taniya"));
-console.log(loginUserMessage());
+// // console.log(loginUserMessage("taniya"));
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1,val2,...num1) // ... ya 3 dots ko spread operator and rest operator bola jatta ha
+{
+    return num1
+
+}
+// 
+const user={
+
+    username:"taniya",
+    price:199
+}
+function handleObject(anyobject)
+{
+    console.log(`use name is ${anyobject.username} and price is${anyobject.price}`);
+}
+handleObject(user)
+handleObject({
+username:"sam",
+price:300
+
+})
+const myNewArray=[234,434,534,200]
+function returnSecondValue(getArray){
+    return getArray[0]
+
+}
+console.log(returnSecondValue(myNewArray));
